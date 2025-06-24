ce9960c37496e892c61bd392289d5a56801e2934
jabeena <jabina123@gmail.com>
2025-06-24 15:43:46 +0300

    Update API URL and rebuild

---
+++ build/precache-manifest.b0729a2bafb79f9d7777ea7d835cfeb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d686c1fb75dfa74df6d3238425b97578",
+    "url": "/index.html"
+  },
+  {
+    "revision": "224327a524447b55d917",
+    "url": "/static/css/main.17d6c6a3.chunk.css"
+  },
+  {
+    "revision": "62dc13a8445739db5eb0",
+    "url": "/static/js/2.724779a1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.724779a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "224327a524447b55d917",
+    "url": "/static/js/main.596433fc.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
